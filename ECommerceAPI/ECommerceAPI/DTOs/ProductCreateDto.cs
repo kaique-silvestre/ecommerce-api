497678511aa294e661d6bdaaa9e7698974de3de1
Kaique Gonzaga Silvestre <kaique.silvestre.22@gmail.com>
2026-03-09 19:57:23 -0300

    feat(api): implement product controller and configure dbcontext injection

--- ECommerceAPI/ECommerceAPI/DTOs/ProductCreateDto.cs
+++ ECommerceAPI/ECommerceAPI/DTOs/ProductCreateDto.cs
@@ -2,5 +2,10 @@
 {
     public class ProductCreateDto
     {
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public int Stock { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
--- ECommerceAPI/ECommerceAPI/DTOs/ProductResponseDto.cs
+++ ECommerceAPI/ECommerceAPI/DTOs/ProductResponseDto.cs
@@ -2,5 +2,12 @@
 {
     public class ProductResponseDto
     {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public int Stock { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
